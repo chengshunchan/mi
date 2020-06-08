d5e4e846aecff262a7fceef467aeb3c9572cb1be
chengshunchan <chengshunchan@qq.com>
2020-06-09 00:45:48 +0800

    'first_commit'

---
+++ mi/js/index.js
@@ -0,0 +1,7 @@
+
+
+// function changeImg(a){
+//     $('.img-list li').eq(a).fadeIn().siblings().fadeOut();
+//     $('.pointer a').eq(a).addClass('cur').siblings().removeClass('cur');
+// }
+// changeImg(0)
